106b41589739d242ef5d2a7ce71d0685074f2703
aron <aronsotomarino@gmail.com>
2024-03-21 20:40:13 -0500

    console ping added

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,9 +2,12 @@ import { pool } from '../config/db.js'
 
 export const ping = async (req, res) => {
   try {
+    console.log('ping recibido')
     const [result] = await pool.query('SELECT "pong"AS result')
     res.json(result[0])
+    console.log('pong enviado')
   } catch (error) {
+    console.log('fallo en db')
     return res.status(500).json({ message: 'Someting goes wrong' })
   }
 }
